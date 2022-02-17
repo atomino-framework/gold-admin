a5a6e51fc6fcb7d3593b45faa90951cfa1e864c6
Gergely Laborci <gergely@laborci.hu>
2022-02-17 20:30:34 +0100

    string input disabled added

--- form-input/string/control.ts
+++ form-input/string/control.ts
@@ -4,4 +4,11 @@ import Component from "./component.svelte"
 @component(Component)
 @layout("row")
 export default class StringControl extends AbstractControl {
+
+	public disabled = false;
+
+	setDisabled(disabled = true){
+		this.disabled = disabled;
+		return this;
+	}
 }
